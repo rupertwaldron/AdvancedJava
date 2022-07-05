bbd6bfd70fff8c59f5747afa4a6b235d5a6808c0
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-07-05 08:05:52 +0100

    added callback modified

--- src/com/ruppyrup/callbacks/CallBackTest.java
+++ src/com/ruppyrup/callbacks/CallBackTest.java
@@ -1,15 +1,22 @@
 package com.ruppyrup.callbacks;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class CallBackTest {
 
   public static void main(String[] args) throws InterruptedException {
     Bob bob = new Bob();
+    CallBack trev = new Trev();
     CallBackTest ct = new CallBackTest();
 
     ct.countToX(10, bob::sayHello);
+
+    ct.doSomething(friend -> System.out.println("Anon says hello to " + friend),
+        CallBack::sayHello, "Rupert");
+
+    ct.doSomething(trev, CallBack::sayHello, "Rupert");
 
   }
 
@@ -20,11 +27,28 @@ public class CallBackTest {
     callback.accept("Simon");
   }
 
+  public void doSomething(CallBack callback, BiConsumer<CallBack, String> consumer, String name) {
+    consumer.accept(callback, name);
+  }
+
 
 }
 
-class Bob {
-  private String name = "Trevor";
+interface CallBack {
+  void sayHello(String friend);
+}
+
+class Bob implements CallBack {
+  private String name = "Bob";
+  @Override
+  public void sayHello(String friend) {
+    System.out.println("Hello " + friend + " my name is "+ name + ".");
+  }
+}
+
+class Trev implements CallBack {
+  private String name = "Trev";
+  @Override
   public void sayHello(String friend) {
     System.out.println("Hello " + friend + " my name is "+ name + ".");
   }
